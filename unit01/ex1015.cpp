31562c01529a06017b632ab96ba514077bda0b16
leo <leo.tsai2011@hotmail.com>
2020-01-14 19:50:44 +0800

    First exercises.

---
+++ unit01/ex1015.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cout << "In theory, theory and practice are the same." << endl;
+    cout << "In practice, they're not." << endl;
+    return 0;
+}
+
