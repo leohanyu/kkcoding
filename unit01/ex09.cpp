4855d3e2870ff7ce3bd08304ad6f079612d9a8d2
leo <leo.tsai2011@hotmail.com>
2020-01-14 20:22:19 +0800

    Complete the first exercises.

--- unit01/ex09.cpp
+++ unit01/ex09.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main() {
-    cout << "    ." << endl;
-    cout << "   . ." << endl;
-    cout << "  .   ." << endl;
-    cout << "   . ." << endl;
-    cout << "    ." << endl;
+    cout << "   *" << endl;
+    cout << "  ***" << endl;
+    cout << " *****" << endl;
+    cout << "*******" << endl;
+    cout << " *****" << endl;
+    cout << "  ***" << endl;
+    cout << "   *" << endl;
     return 0;
 }
-
